e3e40eb4553cfee86aa639b4e595c8aff2933c09
best <2907647445@qq.com>
2023-07-12 19:32:11 +0800

    7/12

--- app.js
+++ app.js
@@ -8,23 +8,24 @@ const logger = require('koa-logger')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
+const comments = require('./routes/comments')
 
 // error handler
 onerror(app)
 
-// middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+// middlewares 中间件
+app.use(bodyparser({ // request body 转换
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(require('koa-static')(__dirname + '/public')) // 静态文件服务
 
-app.use(views(__dirname + '/views', {
+app.use(views(__dirname + '/views', { // 服务端模板引擎
   extension: 'pug'
 }))
 
-// logger
+// logger 打印当前请求所花费的时间
 app.use(async (ctx, next) => {
   const start = new Date()
   await next()
@@ -32,9 +33,22 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
-// routes
+// 模拟登录
+// app.use(async (ctx, next) => {
+//   const query = ctx.query
+//   if (query.username == 'peng') {
+//     await next() // 执行下一个中间件
+//   } else {
+//     // 登录失败
+//     ctx.body = '登录失败'
+//   }
+// })
+
+// routes 注册路由
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(comments.routes(), comments.allowedMethods())
+// allowedMethods 对404或者返回是空的情况的一种补充
 
 // error-handling
 app.on('error', (err, ctx) => {
